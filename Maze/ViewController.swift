693b97eac71f5164634dd9795b7db5c8678b3d34
YochiyanXc <Kashima.Yochiyan@icloud.com>
2023-04-03 16:22:52 +0900

    終盤か！？疲れたぁ

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController {
                     goalView = createView(x: x, y: y, width: cellWidth, height: cellHeight, offsetX: cellOffsetX, offsetY: cellOfsetY)
                     goalView.backgroundColor = UIColor.red
                     view.addSubview(goalView)
-                
+                    
                     
                 default:
                     break
@@ -69,8 +69,8 @@ class ViewController: UIViewController {
             }
             
         }
-            //olayerView 生成
-        playerView = UIView(frame: CGRect(x: 0, y: 0, width: cellWidth, height: cellHeight / 6))
+        //olayerView 生成
+        playerView = UIView(frame: CGRect(x: 0, y: 0, width: cellWidth / 6, height: cellHeight / 6))
         playerView.center = startView.center
         playerView.backgroundColor = UIColor.gray
         view.addSubview(playerView)
@@ -81,6 +81,45 @@ class ViewController: UIViewController {
         
         startAccelemeter()
         
+        
+        func startAccelemeter() {
+            playerMotionManager.startAccelerometerUpdates(to: OperationQueue.current!){ data, err in
+                self.speedX += (data?.acceleration.x)!
+                self.playerView.center.x += self.speedX
+                self.speedY += (data?.acceleration.y)!
+                self.playerView.center.y -= self.speedY
+                
+                if self.playerView.frame.origin.x < 0{
+                    self.playerView.frame.origin.x = 0
+                    self.speedX = 0
+                }
+                else if self.playerView.frame.origin.x > self.view.frame.width - self.playerView.frame.width {
+                    self.playerView.frame.origin.x = self.view.frame.width - self.playerView.frame.width
+                    self.speedX = 0
+                }
+                
+                if self.playerView.frame.origin.y < 0{
+                    self.playerView.frame.origin.y = 0
+                    self.speedY = 0
+                }
+                else if self.playerView.frame.origin.y > self.view.frame.height - self.playerView.frame.height {
+                    self.playerView.frame.origin.y = self.view.frame.height - self.playerView.frame.height
+                    self.speedY = 0
+                }
+                for wallRect in self.wallRectArray {
+                    if wallRect.intersects(self.playerView.frame) {
+                        self.gameCheck(result: "GameOver", message: "壁に当たったよ")
+                        return
+                    }
+                    
+                }
+                if self.goalView.frame.intersects(self.playerView.frame) {
+                    self.gameCheck(result: "clear", message: "クリアしたよ！")
+                    return
+                }
+                
+            }
+        }
         
         func createView(x: Int, y: Int, width: CGFloat, height: CGFloat, offsetX: CGFloat, offsetY: CGFloat) ->
         UIView {
@@ -93,6 +132,30 @@ class ViewController: UIViewController {
             
             return view
         }
+        func retty() {
+            //位置初期化
+            playerView.center = startView.center
+            //センサースタート
+            if !playerMotionManager.isAccelerometerActive {
+                startAccelemeter()
+            }
+            //speed初期化
+            speedX = 0.0
+            speedY = 0.0
+        }
+        func gameCheck(result: String, message: String) {
+            //加速度とめる
+            if playerMotionManager.isAccelerometerActive {
+                playerMotionManager.stopAccelerometerUpdates()
+            }
+            let gameCheckAlert: UIAlertController = UIAlertController(title: result, message: message, preferredStyle: .alert)
+            
+            let retryAction = UIAlertAction(title: "もう一度", style: .default, handler:  {
+        (action: UIAlertAction!) -> Void in
+                self.retty()
+            })
+            gameCheckAlert.addAction(retryAction)
+            self.present(gameCheckAlert, animated: true, completion: nil)
+        }
     }
-    
 }
